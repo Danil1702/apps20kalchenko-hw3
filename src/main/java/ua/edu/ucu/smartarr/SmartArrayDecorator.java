ead1e7357b63919332ab9a7d33225c844a607225
Danil1702 <60688896+Danil1702@users.noreply.github.com>
2020-11-27 13:55:30 +0200

    Update SmartArrayDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/SmartArrayDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/SmartArrayDecorator.java
@@ -1,12 +1,10 @@
 package ua.edu.ucu.smartarr;
 
-import java.util.Arrays;
-
 abstract class SmartArrayDecorator implements SmartArray {
 
     protected SmartArray smartArray;
 
     public SmartArrayDecorator(SmartArray smartArray) {
-        this.smartArray = Arrays.copyOf(smartArray, smartArray.length);
+        this.smartArray = smartArray;
     }
 }
